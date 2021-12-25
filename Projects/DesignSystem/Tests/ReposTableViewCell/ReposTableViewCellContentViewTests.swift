5e8bc2af3bd0e368cf9c491b4c0cdde858db7e14
aaa-carrr <96629817+aaa-carrr@users.noreply.github.com>
2021-12-25 17:44:27 -0300

    Add ReposTableViewCell tests

---
+++ Projects/DesignSystem/Tests/ReposTableViewCell/ReposTableViewCellContentViewTests.swift
@@ -0,0 +1,54 @@
+import XCTest
+import RxSwift
+import SnapshotTesting
+@testable import DesignSystem
+
+final class ReposTableViewCellContentViewTests: XCTestCase {
+    var sut: ReposTableViewCellContentView!
+    
+    override func setUp() {
+        super.setUp()
+        sut = ReposTableViewCellContentView(frame: .zero)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+    }
+    
+    func testReposTableViewCellContentView_when_itIsUpdated() {
+        let loadableImage = LoadableImage(
+            single: Single<UIImage>.create { single in
+                single(
+                    .success(
+                        UIImage(systemName: "person.circle.fill") ?? UIImage()
+                    )
+                )
+                return Disposables.create { }
+            },
+            defaultImage: UIImage()
+        )
+        
+        let repoAuthor = UserProfileCompactViewModel(
+            username: "aaa-carrr",
+            profileImage: loadableImage
+        )
+        
+        let viewModel = ReposTableViewCellViewModel(
+            repoName: "Repository",
+            repoDescription: "Repo description with some text to describe what it does. Repo description with some text to describe what it does. Repo description with some text to describe what it does. Repo description with some text to describe what it does",
+            repoAuthor: repoAuthor,
+            repoStars: "34 stars",
+            repoForks: "6 forks"
+        )
+        
+        sut.update(with: viewModel)
+        
+        let size = CGSize(
+            width: 350,
+            height: 110
+        )
+        
+        assertSnapshot(matching: sut, as: .image(size: size))
+    }
+}
